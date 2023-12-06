bfa7ce480d76547b5e6ab0be7d19b119200ab0ad
Muhamad Hewa Rahim <146334970+MHRGA@users.noreply.github.com>
2023-12-06 19:58:55 +0000

    Task 3 Started

--- coursework/task3/FitnessDataSorter.c
+++ coursework/task3/FitnessDataSorter.c
@@ -25,29 +25,21 @@ void tokeniseRecord(const char *input, const char *delimiter, char *date, char *
     free(inputCopy);
 }
 
-void read_from_file(FILE *file, FITNESS_DATA data, char filename[])
+void read_from_file(FILE *file, FITNESS_DATA *data, char filename[])
 {
     int i = 0;
-    int length = 1;
-    FITNESS_DATA *array;
-    array = malloc(sizeof(data) * length);
-	int buffer_size = 100;
-    char line_buffer[buffer_size];
     strcat(filename, ".tsv");
     FILE *newfile = fopen(filename, "w");
-    while (fgets(line_buffer, buffer_size, file) != NULL) {
-        char dataline[25];
-        strcpy(dataline, line_buffer);
-        char* my_delimiter = ",";
-        char my_steps[5];
-        tokeniseRecord(dataline, my_delimiter, array[i].date, array[i].time, my_steps);
-        array[i].steps = atoi(my_steps);
-		length++;
-        array = realloc(array, sizeof(data) * length);
-        fprintf(newfile, "%s\t%s\t%d \n", array[i].date, array[i].time, array[i].steps);
+	int buffer_size = 100;
+    char line_buffer[buffer_size];
+    while (fgets(line_buffer, buffer_size, file) != NULL) 
+    {
+        char my_steps[10];
+        tokeniseRecord(line_buffer, ",", data[i].date, data[i].time, my_steps);
+        data[i].steps = atoi(my_steps);
+        fprintf(newfile, "%s\t%s\t%d \n", data[i].date, data[i].time, data[i].steps);
         i++;
 	}
-    fclose(file);
 }
 
 FILE *open_file(char filename[], char mode[])
@@ -62,7 +54,7 @@ FILE *open_file(char filename[], char mode[])
 
 int main() {
     char filename[100];
-    FITNESS_DATA data;
+    FITNESS_DATA data[500];
     printf("Enter Filename: ");
     scanf("%s", filename);
     FILE *file = open_file(filename, "r");
